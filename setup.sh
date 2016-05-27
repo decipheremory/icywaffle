bd1e5b634d0200307c6f953ccb53ffa62d129611
Emory Merryman <emory.merryman@deciphernow.com>
2016-05-27 17:25:06 +0000

    adding setup scripts

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 ln --symbolic --force ${HOME}/bin ${HOME}/workspace/bin &&
+setup_personal.sh frozenhurricane ssh://git@gitlab.363-283.io:2252/emory.merryman/frozenhurricane.git &&
+true
