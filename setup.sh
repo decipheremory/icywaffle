2c24f5b0fee7b66b420786a0ef7e905f0538d0ac
Emory Merryman <emory.merryman@deciphernow.com>
2016-05-27 17:28:30 +0000

    reorig setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-ln --symbolic --force ${HOME}/bin ${HOME}/workspace/bin &&
+# ln --symbolic --force ${HOME}/bin ${HOME}/workspace/bin &&
+
+setup_personal.sh needlesslaser ssh://git@gitlab.363-283.io:2252/emory.merryman/needlesslaser.git &&
+setup_personal.sh icywaffle ssh://git@gitlab.363-283.io:2252/emory.merryman/icywaffle.git &&
 setup_personal.sh frozenhurricane ssh://git@gitlab.363-283.io:2252/emory.merryman/frozenhurricane.git &&
 true
