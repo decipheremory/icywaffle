2bf58ee87db0c7785e764cae109225fe1c822132
Emory Merryman <emory.merryman@deciphernow.com>
2016-05-27 17:29:35 +0000

    specify origin url

--- setup_personal.sh
+++ setup_personal.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 NAME="${1}" &&
+ORIGIN_URL="${2}" &&
 mkdir "${HOME}/workspace/${NAME}" &&
 git -C "${HOME}/workspace/${NAME}" init &&
 git -C "${HOME}/workspace/${NAME}" remote add origin "${ORIGIN_URL}" &&
