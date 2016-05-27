5a3b408f8bbd63b6f4768dad850a5d9e24a7fa21
Emory Merryman <emory.merryman@deciphernow.com>
2016-05-27 17:31:22 +0000

    add post-commit hook

--- setup_personal.sh
+++ setup_personal.sh
@@ -6,4 +6,5 @@ mkdir "${HOME}/workspace/${NAME}" &&
 git -C "${HOME}/workspace/${NAME}" init &&
 git -C "${HOME}/workspace/${NAME}" remote add origin "${ORIGIN_URL}" &&
 git -C "${HOME}/workspace/${NAME}" pull origin develop &&
+ln --symbolic --force /opt/needlesslaser/bin/post-commit.sh "${HOME}/workspace/${NAME}/.git/hooks/post-commit" && 
 true
